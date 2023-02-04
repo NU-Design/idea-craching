ad9798ff2d8cf26444ed25f7a55f4fddca6ca3cb
Dexian Chen <orangemoon@Dexians-MBP.hsd1.ca.comcast.net>
2023-02-04 05:24:11 -0800

    home page sketch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,23 @@
 import ResponsiveAppBar from "../navbar/ResponsiveAppBar";
+import RecipeReviewCard from "../../common/components/PostCard";
+import './index.css';
+//import { style } from "@mui/system";
 
 const Home = () => {
   return (
     <>
         <ResponsiveAppBar/>
-        <p>This is a Home page</p>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            pt:"10px"
+        }}>
+            <div className="flexbox-container">
+                <div><RecipeReviewCard props={false}/></div>
+                <div><RecipeReviewCard props={false}/></div>
+                <div><RecipeReviewCard props={false}/></div>
+            </div>
+        </div>
     </>
   );
 };
