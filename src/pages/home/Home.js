8446cc65995d6eb928afb70f02778de8fe420956
Dexian Chen <orangemoon@Dexians-MBP.hsd1.ca.comcast.net>
2023-02-04 03:56:56 -0800

    solve conflict

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import ResponsiveAppBar from "../navbar/ResponsiveAppBar";
+
+const Home = () => {
+  return (
+    <>
+      <ResponsiveAppBar/>
+      <p>post card</p>
+    </>
+  );
+};
+
+export default Home;
