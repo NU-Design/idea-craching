ba93efb3788337950073ec1b869262c145cd70c1
Jinxin Hu <hujinxin0607@gmail.com>
2023-02-04 03:10:39 -0700

    new feature: GetUserRole, GetUserByID, Get

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ResponsiveAppBar from './pages/ResponsiveAppBar';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import IdeaChain from './pages/idea_chain/IdeaChain';
+import HJXTest from './common/services/db/hjxTest';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/ideachain_test" element={<IdeaChain />} />
           <Route path="/responsivebar_test" element={<ResponsiveAppBar />} />
+          <Route path="/hjx" element={<HJXTest />} />
         </Routes>
       </BrowserRouter>
     </>
