53b8b734be0b36a54672193328be8dd3d915476b
Jinxin Hu <hujinxin0607@gmail.com>
2023-02-04 01:17:30 -0700

    user and auth module

---
+++ src/common/services/db/auth.js
@@ -0,0 +1,72 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+
+import { collection, serverTimestamp, addDoc } from 'firebase/firestore';
+
+import { db } from '../../utils/firebase/firebase.util';
+
+import { emptyMessage } from './value/string';
+
+export const auth = getAuth();
+
+export const createUserDocumentFromAuth = async (userAuth, data) => {
+  if (!userAuth) return;
+
+  data['timestamp'] = serverTimestamp();
+  data['user_id'] = userAuth.uid;
+  data['email'] = userAuth.email;
+  data['bio'] = emptyMessage;
+  data['liked_ideas'] = [];
+  data['owned_ideas'] = [];
+
+  console.log('DATA', data);
+  try {
+    const docRef = await addDoc(collection(db, 'users'), data);
+    return docRef;
+  } catch (error) {
+    console.log('error creating the user', error.message);
+  }
+};
+
+export const createAuthUserWithEmailAndPassword = async (
+  email,
+  password,
+  data,
+) => {
+  if (!email || !password) return;
+
+  try {
+    const authObject = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    ).then((result, reject) => {
+      return result;
+    });
+    const userInfo = {
+      uid: authObject.user.uid,
+      email: authObject.user.email,
+    };
+    createUserDocumentFromAuth(userInfo, data);
+    return userInfo;
+  } catch (error) {
+    console.log('error createAuthUserWithEmailAndPassword', error.message);
+  }
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  try {
+    const result = await signInWithEmailAndPassword(auth, email, password);
+    console.log('success signIn');
+    return result;
+  } catch (error) {
+    console.log('error signInAuthUserWithEmailAndPassword', error.message);
+  }
+};
+
+export const signOutUser = async () => await signOut(auth);
