c926c89982ae73a2c3e8d51409f2302c826a1fb5
Dexian Chen <orangemoon@Dexians-MBP.hsd1.ca.comcast.net>
2023-02-03 16:36:28 -0800

    post card sample

--- src/common/components/PostCard.js
+++ src/common/components/PostCard.js
@@ -1,27 +1,100 @@
 // import StartFunctionOnClick from "./on_click";
 
-const PostCard = (
-    {
-        card =   {
-            "id": 123,
-            "username": "SpaceX",
-            "email": "testEmail@gmail.com",
-            "password": 123456,
-            "confirmPassword": 123456,
-            "gender": "nonbinary",
-            "bio": "This is a sample biography of the user",
-            "avatar": "spacex.png",
-            "following": true,
-            "folloers": 520,
-            "role": "designer/developer",
-            "postImage": "starship.jpg",
-          }
-    }
-) => {    
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-    return (
-        <div>
-            <p> Post Card test</p>
-        </div>
-    );
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function RecipeReviewCard() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="/static/images/cards/paella.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          111
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            222
+          </Typography>
+          <Typography paragraph>
+            333
+          </Typography>
+          <Typography paragraph>
+            444
+          </Typography>
+          <Typography>
+            555
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
