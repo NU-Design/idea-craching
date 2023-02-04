6227d43f881f187f96dbd7d7b06e2faf9759de41
gyouzazuoyg <gyouza_azuoyg@outlook.com>
2023-02-03 17:25:37 -0800

    Added IdeaChain skeleton

--- src/pages/idea_chain/IdeaChain.js
+++ src/pages/idea_chain/IdeaChain.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Chrono } from 'react-chrono';
 
 function IdeaChain() {
-  return <div>Some Demo Text</div>;
+  const items = [
+    {
+      title: 'May 1940',
+      cardTitle: 'Dunkirk',
+      url: 'https://www.w3schools.com/images/w3schools_green.jpg',
+      cardSubtitle:
+        'Men of the British Expeditionary Force (BEF) wade out to..',
+      cardDetailedText:
+        'Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..',
+      media: {
+        type: 'IMAGE',
+        source: {
+          url: 'https://www.w3schools.com/images/w3schools_green.jpg',
+        },
+      },
+    },
+    {
+      title: 'May 1950',
+      cardTitle: 'DDD',
+      url: 'https://www.w3schools.com/images/w3schools_green.jpg',
+      cardSubtitle:
+        'Men of the British Expeditionary Force (BEF) wade out to..',
+      cardDetailedText:
+        'Men of the British Expeditionary Force (BEF) wade out to..',
+      media: {
+        type: 'IMAGE',
+        source: {
+          url: 'https://www.w3schools.com/images/w3schools_green.jpg',
+        },
+      },
+    },
+  ];
+  return (
+    <div style={{ width: '500px', height: '900px' }}>
+      <Chrono items={items} />
+    </div>
+  );
 }
 
 export default IdeaChain;
