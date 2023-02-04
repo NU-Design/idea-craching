0b309a34d1314a8925a09b6fb853e1027ab3b926
gyouzazuoyg <gyouza_azuoyg@outlook.com>
2023-02-04 14:24:59 -0800

    Final demo

--- src/pages/idea_chain/ideaChain.jsx
+++ src/pages/idea_chain/ideaChain.jsx
@@ -1,52 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Chrono } from 'react-chrono';
 import ResponsiveAppBar from '../navbar/ResponsiveAppBar';
 import MarkdownViewer from '../markdown_viewer/MarkdownViewer';
+import { getIdeasByAncestorId } from '../../common/services/db/ideas';
 
 const LOGO =
   'https://firebasestorage.googleapis.com/v0/b/idea-crashing.appspot.com/o/logo.png?alt=media&token=9c8cd5a4-792b-4835-aee3-68fa2641acec';
 
+const itemsDemo = [
+  {
+    title: 'Jan 1st 2023',
+    cardTitle: 'Regarding Refactoring Redux',
+    url: LOGO,
+    cardSubtitle: 'I plan to refactor all states into redux states and reducers',
+    media: {
+      type: 'IMAGE',
+      source: {
+        url: LOGO,
+      },
+    },
+    mdText: '# A sample original JavaScript code is shown here:\n\n~~~js\nsetItems(newVal);\nconsole.log("New Value", newVal);\n~~~\n',
+  },
+  {
+    title: 'Jan 24ed 2023',
+    cardTitle: 'I suggest you refactor this way',
+    url: LOGO,
+    cardSubtitle: 'I wrote some code for you.',
+    media: {
+      type: 'IMAGE',
+      source: {
+        url: LOGO,
+      },
+    },
+    mdText: '# A sample original JavaScript code is shown here：\n\n~~~js\ndispatch(action: "UI/setNewItems", payload(newVal));\nconsole.log("New Value", newVal);\n~~~\n',
+  },
+];
+
 function IdeaChain() {
-  const items = [
-    {
-      title: 'May 1940',
-      cardTitle: 'Dunkirk',
-      url: LOGO,
-      cardSubtitle: 'brief1',
-      cardDetailedText:
-        'Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..Men of the British Expeditionary Force (BEF) wade out to..',
-      media: {
-        type: 'IMAGE',
-        source: {
-          url: LOGO,
+  const [items, setItems] = useState([]);
+
+  const asyncGetChainedIdeas = async () => {
+    let data = await getIdeasByAncestorId('down-iid');
+
+    // // const data = await getIdeasByAncestors;
+    data.forEach((element) => {
+      return {
+        title: element['timestamp'] ? element['timestamp'] : 'TBD',
+        cardTitle: element['title'] ? element['title'] : '',
+        url: element['url'] ? element['url'] : '',
+        cardSubtitle: element['brief'] ? element['brief'] : '',
+        media: {
+          type: 'IMAGE',
+          source: {
+            url: element['media']['source'] ? element['media']['source'] : LOGO,
+          },
         },
-      },
-    },
-    {
-      title: 'May 1950',
-      cardTitle: 'DDD',
-      url: LOGO,
-      cardSubtitle: 'brief2',
-      media: {
-        type: 'IMAGE',
-        source: {
-          url: LOGO,
-        },
-      },
-    },
-  ];
+        mdText: element['details'] ? element['details'] : '',
+      };
+    });
+    setItems([...data]);
+    console.log('items:', items);
+  };
+
+  useEffect(() => {
+    asyncGetChainedIdeas();
+  }, []);
 
   return (
     <>
       <ResponsiveAppBar />
       <div style={{ width: '100%', height: '85vh' }}>
-        <Chrono items={items}>
-          {items.map((item, index) => (
-            <div style={{ width: '100%' }}>
-              <hr />
-              <MarkdownViewer />
-            </div>
-          ))}
+        <Chrono items={itemsDemo}>
+          {itemsDemo.map((item, index) => {
+            console.log('Item to be renderd:', item);
+            return (
+              <div key={index} style={{ width: '100%' }}>
+                <hr />
+                <MarkdownViewer markdown={item.mdText} />
+              </div>
+            );
+          })}
         </Chrono>
       </div>
     </>
