e5d853b4c5ef17845cb2a229a8c4fc395956d6cb
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-02-04 14:44:20 -0800

    profile page

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Base from '../base_outline/base';
 import ResponsiveAppBar from '../navbar/ResponsiveAppBar';
 
+const urlLink = "https://firebasestorage.googleapis.com/v0/b/idea-crashing.appspot.com/o/profile.png?alt=media&token=aeef9915-5f15-4394-901f-94805a956384";
+
+
 class Profile extends Component {
   state = {};
   render() {
     return (
       <>
         <ResponsiveAppBar />
-        <div><h1>This is a Profile page. </h1></div>
+        <div style={{margin: 'auto', display: "flex"}}>
+        <img src={urlLink} style={{ margin: "auto", width: '70%', height: '70%' }} /></div>
       </>
     );
   }
