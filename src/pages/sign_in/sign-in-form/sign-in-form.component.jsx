aaabad868c9ef111e10de8f5c04ea4073a0a0768
SitingT <tang.sit@northeastern.edu>
2023-02-04 00:24:56 -0800

    new feature: sign in form

---
+++ src/pages/sign_in/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+import FormInput from '../form-input/input.component';
+import Button from '../button/button.component';
+
+import './sign-in-form.styles.scss';
+
+const defaultFormFields = {
+  email: '',
+  password: '',
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className='sign-up-container'>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
