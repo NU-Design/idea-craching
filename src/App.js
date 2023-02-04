7e2a8aad5f47ab9d538ae3e74ddcaff2af54c6dd
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-02-03 16:20:55 -0800

    create NavBar

--- src/App.js
+++ src/App.js
@@ -1,49 +1,66 @@
-import ReactMarkDownDemo from "./common/components/react_mark_down_demo";
+import React, { Component } from 'react';
+import ResponsiveAppBar from './pages/ResponsiveAppBar';
 
-const App = () => {
-
-
-  return (
-    <div className="ideaboxes-container ">
-      <div className="ideabox-container">
-        <div className="ideabox-body-container">
-          {/*<img />*/}
-          <div className="ideabox-container">
-            <h2> title </h2>
-            <p>descreption</p>
-          </div>
-        </div>
-      </div>
-      <div className="ideabox-container">
-        <div className="ideabox-body-container">
-          {/*<img />*/}
-          <div className="ideabox-container">
-            <h2> title </h2>
-            <p>descreption</p>
-          </div>
-        </div>
-      </div>
-      <div className="ideabox-container">
-        <div className="ideabox-body-container">
-          {/*<img />*/}
-          <div className="ideabox-container">
-            <h2> title </h2>
-            <p>descreption</p>
-          </div>
-        </div>
-      </div>
-      <div className="ideabox-container">
-        <div className="ideabox-body-container">
-          {/*<img />*/}
-          <div className="ideabox-container">
-            <h2> title </h2>
-            <p>descreption</p>
-          </div>
-        </div>
-      </div>
-      <ReactMarkDownDemo />
-    </div>
-  );
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <React.Fragment>
+        <ResponsiveAppBar />
+      </React.Fragment>
+    );
+  }
 }
-
+ 
 export default App;
+
+
+// import ReactMarkDownDemo from "./common/components/react_mark_down_demo";
+
+// const App = () => {
+
+
+//   return (
+//     <div className="ideaboxes-container ">
+//       <div className="ideabox-container">
+//         <div className="ideabox-body-container">
+//           {/*<img />*/}
+//           <div className="ideabox-container">
+//             <h2> title </h2>
+//             <p>descreption</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="ideabox-container">
+//         <div className="ideabox-body-container">
+//           {/*<img />*/}
+//           <div className="ideabox-container">
+//             <h2> title </h2>
+//             <p>descreption</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="ideabox-container">
+//         <div className="ideabox-body-container">
+//           {/*<img />*/}
+//           <div className="ideabox-container">
+//             <h2> title </h2>
+//             <p>descreption</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="ideabox-container">
+//         <div className="ideabox-body-container">
+//           {/*<img />*/}
+//           <div className="ideabox-container">
+//             <h2> title </h2>
+//             <p>descreption</p>
+//           </div>
+//         </div>
+//       </div>
+//       <ReactMarkDownDemo />
+//     </div>
+//   );
+// }
+
+// export default App;
