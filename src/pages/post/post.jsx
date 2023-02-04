851c7140b9427e36e0554ac3abe20498d5db877e
Jinxin Hu <hujinxin0607@gmail.com>
2023-02-04 05:14:06 -0700

    Update post.jsx

--- src/pages/post/post.jsx
+++ src/pages/post/post.jsx
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
-=======
 import React, { Component } from 'react';
 import Base from '../base_outline/base';
 import ResponsiveAppBar from '../navbar/ResponsiveAppBar';
 
 class Post extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <>
-              <ResponsiveAppBar />
-              <div>Some Demo Text IdeaChain</div>
-            </>
-          );
-    }
+  state = {};
+  render() {
+    return (
+      <>
+        <ResponsiveAppBar />
+        <div>Some Demo Text IdeaChain</div>
+      </>
+    );
+  }
 }
- 
+
 export default Post;
->>>>>>> will
